6083c6e04e6adb4db810b66bb77ca17e8f7c6e2e
cevat <cevatarmutlu@gmail.com>
2019-05-12 11:48:52 +0300

    MyThread update

--- MyThread.py
+++ MyThread.py
@@ -2,11 +2,13 @@ import threading
 
 
 class MyThread(threading.Thread):
-    def __init__(self, func_name):
+    def __init__(self, func_name, first, last):
         threading.Thread.__init__(self)
         self.func_name = func_name
+        self.first = first
+        self.last = last
 
     def run(self):
-        self.func_name()
+        self.func_name(self.first, self.last)
 
 
