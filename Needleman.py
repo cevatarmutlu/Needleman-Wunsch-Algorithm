36a6ac70bc4aa8ba81d9a87cbb05e0fb76345759
cevat <cevatarmutlu@gmail.com>
2019-05-12 10:24:31 +0300

    duzeltme

--- Needleman.py
+++ Needleman.py
@@ -1,64 +1,32 @@
-class Needleman:
-    def __init__(self, match, mis_match, gap):
-        self.dna1 = ''
-        self.dna2 = ''
-        self.match = match
-        self.mis_match = mis_match
-        self.gap = gap
-        self.gap_list = []
-        self.l_val_list = []
-        self.u_val_list = []
-        self.length = 0
+def calculate(dna1: str, dna2: str, gap: float, match: float, mismatch: float):
+    row_len = len(dna2) + 1
+    col_len = len(dna1) + 1
+    need_arr = create_list(row_len, col_len, gap)
 
-    def set_dna(self, dna1, dna2):
-        self.dna1 = dna1
-        self.dna2 = dna2
-        self.length = len(dna1) + 1
+    for row in range(1, row_len):
+        for col in range(1, col_len):
+            left_val = need_arr[row][col - 1] + gap
+            down_val = need_arr[row - 1][col] + gap
+            if dna2[row - 1] == dna1[col - 1]:
+                match_value = match
+            else:
+                match_value = mismatch
+            corner_val = need_arr[row - 1][col - 1] + match_value
 
-    def __create_list(self, arr):
-        temp_row = [x * self.gap for x in range(self.length)]
-        arr.append(temp_row)
-        for i in range(1, self.length):
-            arr.append([arr[i - 1][0] + self.gap])
+            max_val = max([left_val, down_val, corner_val])
+            need_arr[row].append(max_val)
 
-    def __comp_l_val_list(self):
-        # l_val_list' in kalan değerlerini tamamlıyor
-        for i in range(1, len(self.l_val_list)):
-            for j in range(1, len(self.l_val_list[0])):
-                left_value = self.l_val_list[i][0] + self.gap * j
-                self.l_val_list[i].append(left_value)
+    return need_arr[row_len - 1][col_len - 1]
 
-    def __comp_u_val_list(self):
-        # u_val_list' in kalan değerlerini tamamlıyor
-        for i in range(1, len(self.u_val_list)):
-            for j in range(1, len(self.u_val_list[0])):
-                up_value = self.gap_list[0][j] + self.gap * i
-                self.u_val_list[i].append(up_value)
 
-    def gap_list_create(self):
-        self.__create_list(self.gap_list)
+def create_list(row_len: int, col_len: int, gap: float):
+    arr = []
+    temp_row = [x * gap for x in range(col_len)]
+    arr.append(temp_row)
+    for i in range(1, row_len):
+        arr.append([arr[i - 1][0] + gap])
+    return arr
 
-    def l_val_list_create(self):
-        self.__create_list(self.l_val_list)
-        self.__comp_l_val_list()
 
-    def u_val_list_create(self):
-        self.__create_list(self.u_val_list)
-        self.__comp_u_val_list()
-
-    def calculate(self):
-        for i in range(1, self.length):
-            for j in range(1, self.length):
-                left_value = self.l_val_list[i][j]
-                up_value = self.u_val_list[i][j]
-                if self.dna2[i - 1] == self.dna1[j - 1]:
-                    match_val = self.match
-                else:
-                    match_val = self.mis_match
-                val = left_value + up_value + match_val + self.gap_list[i - 1][j - 1]
-                self.gap_list[i].append(val)
-
-    def get_score(self):
-        return self.gap_list[self.length - 1][self.length - 1]
 
 
--- Test.py
+++ Test.py
@@ -1,45 +1,24 @@
 import Fasta
 import Needleman
-import MyThread
-
-
-def create_thread(arr: list):
-    arr.clear()
-    arr.append(MyThread.MyThread(needleman.gap_list_create))
-    arr.append(MyThread.MyThread(needleman.l_val_list_create))
-    arr.append(MyThread.MyThread(needleman.u_val_list_create))
-    return arr
-
+line_list = Fasta.MyFasta('deneme').get_list()
 
 match = 3.621354295
 mis_match = -2.451795405
 gap = -1.832482334
-
 scores = []
-fasta = Fasta.MyFasta('deneme')
-line_list = fasta.get_list()
-
 # num_lines = len(line_list)
-num_lines = 9
-# 9*10/2=45 kere score değeri hesaplıyor. 9 satırın her birinin diğerleri ile score değerini hesaplıyor.
-needleman = Needleman.Needleman(match, mis_match, gap)
-threads = []
+num_lines = 10
 
 for i in range(num_lines):
     for j in range(i + 1, num_lines):
         dna1 = line_list[i]
         dna2 = line_list[j]
-        needleman.set_dna(dna1, dna2)
-        threads = create_thread(threads)
-        for ths in threads:
-            ths.start()
-        for thj in threads:
-            thj.join()
-        needleman.calculate()
-        scores.append(needleman.get_score())
+        score = Needleman.calculate(dna1, dna2, gap, match, mis_match)
 
-# -147572.93959690002
-print(scores)
+        scores.append(['s' + str(i), 's' + str(j), str(score)])
 
-
-
+# 223.91927892400074
+for sc in scores:
+    for item in sc:
+        print('%s' % item, end=" ")
+    print()
